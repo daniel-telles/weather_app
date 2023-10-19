10d236e915f2e9909041e349f9d5c97702f1f26a
daniel-telles <danieltelles7@gmail.com>
2023-10-19 18:07:00 -0300

    home screen background

---
+++ weather_app/lib/features/clima/presentation/views/home_screen.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: const AlignmentDirectional(3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(color: Colors.orange[400]),
+                ),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 100.0,
+                  sigmaY: 100.0,
+                ),
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.transparent),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
