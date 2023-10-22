bae7ffa265b9686c74c95a18451f1f7638ad490e
daniel-telles <danieltelles7@gmail.com>
2023-10-22 20:51:43 -0300

    pronto

--- weather_app/lib/features/clima/presentation/views/helpers/weather_icons.dart
+++ weather_app/lib/features/clima/presentation/views/helpers/weather_icons.dart
@@ -1,29 +1,45 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+String getPeriodOfDay() {
+  DateTime now = DateTime.now();
+  String format = DateFormat.j().format(now);
+  return format;
+}
+
+Image imageToShow() {
+  if (getPeriodOfDay() == 'AM') {
+    return Image.asset('assets/6.png');
+  } else {
+    return Image.asset('assets/moon.png');
+  }
+}
 
 Widget getWeatherIcon(int code) {
-    switch (code) {
-      case > 200 && <= 300:
-        return Image.asset('assets/1.png');
+  switch (code) {
+    case > 200 && <= 300:
+      return Image.asset('assets/1.png');
 
-      case >= 300 && < 400:
-        return Image.asset('assets/2.png');
+    case >= 300 && < 400:
+      return Image.asset('assets/2.png');
 
-      case >= 500 && < 600:
-        return Image.asset('assets/3.png');
+    case >= 500 && < 600:
+      return Image.asset('assets/3.png');
 
-      case >= 600 && < 700:
-        return Image.asset('assets/4.png');
+    case >= 600 && < 700:
+      return Image.asset('assets/4.png');
 
-      case >= 700 && < 800:
-        return Image.asset('assets/5.png');
+    case >= 700 && < 800:
+      return Image.asset('assets/5.png');
 
-      case == 800:
-        return Image.asset('assets/6.png');
+    case == 800:
+      return imageToShow();
 
-      case > 800 && <= 804:
-        return Image.asset('assets/8.png');
 
-      default:
-        return Image.asset('assets/7.png');
-    }
+    case > 800 && <= 804:
+      return Image.asset('assets/8.png');
+
+    default:
+      return Image.asset('assets/7.png');
   }
+}
--- weather_app/lib/features/clima/presentation/views/home_screen.dart
+++ weather_app/lib/features/clima/presentation/views/home_screen.dart
@@ -17,6 +17,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
